64e899c804d3cda0bec4182d64cf0ee741e8c554
pushpraj33 <pushprajkumar640@gmail.com>
2026-09-18 00:06:53 +0530

    simple interest

---
+++ simple_interest.py
@@ -0,0 +1,6 @@
+p = float(input("Enter Principal: "))
+r = float(input("Enter Rate: "))
+t = float(input("Enter Time: "))
+simple_interest = (p * r * t) / 100
+
+print("Simple Interest =", simple_interest)
